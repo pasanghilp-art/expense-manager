003c5a5bd93574540557775987b718f2f2c2784a
Pasang Tamang <pasanghilp@gmail.com>
2026-08-28 19:00:52 +0545

    send expense as child data from maped expense

---
+++ src/components/ExpenseList.jsx
@@ -0,0 +1,17 @@
+import { ExpenseItem } from "./ExpenseItem";
+
+export function ExpenseList({ expenses }) {
+    {
+        if (expenses.length === 0) {
+            return <p>No expenses yet!</p>;
+        }
+    }
+
+    return (
+        <div className="expense-list">
+            {expenses.map((expense) => (
+                <ExpenseItem key={expense.id} expense={expense} />
+            ))}
+        </div>
+    );
+}
