036901565172b0cd040a98f674627f93cec7c531
Pasang Tamang <pasanghilp@gmail.com>
2026-08-29 11:48:54 +0545

    add localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { ExpenseForm } from "./components/ExpenseForm";
 import { ExpenseList } from "./components/ExpenseList";
 
 function App() {
-    const [expenses, setExpenses] = useState([]);
+    const [expenses, setExpenses] = useState(
+        JSON.parse(localStorage.getItem("expenses") || "[]"),
+    );
+
+    useEffect(() => {
+        localStorage.setItem("expenses", JSON.stringify(expenses));
+    }, [expenses]);
 
     const onAddExpense = (formData) => {
         const id = Date.now();
